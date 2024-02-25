3ea4d1e06ff401858a12a3eb9bc60b9efb0f3e55
Brajesh <brajeshkumarsah2010@gmail.com>
2024-02-25 11:59:02 +0530

    commit1

---
+++ src/test/java/pacage1/Class1Test.java
@@ -0,0 +1,20 @@
+package pacage1;
+
+import org.testng.annotations.Test;
+
+public class Class1Test {
+    @Test
+	public void method1() {
+		System.out.println("method 1 is executing");
+	}
+    
+    @Test
+    public void method2() {
+		System.out.println("method 2 is executing");
+	}
+    
+    @Test
+    public void method3() {
+		System.out.println("method 3 is executing");
+	}
+}
---
+++ src/test/java/pacage1/Class2Test.java
@@ -0,0 +1,23 @@
+package pacage1;
+
+import org.testng.annotations.Test;
+
+public class Class2Test {
+
+	
+	
+	  @Test
+		public void method4() {
+			System.out.println("method 4 is executing");
+		}
+	    
+	    @Test
+	    public void method5() {
+			System.out.println("method 5 is executing");
+		}
+	    
+	    @Test
+	    public void method6() {
+			System.out.println("method 6  is executing");
+		}
+}
